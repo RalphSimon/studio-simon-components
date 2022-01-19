e31c72e6879bcd2c5679017ea962f96e5d42b841
Ralph Simon <ralph.simon2012@icloud.com>
2022-01-19 22:20:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Studio Simon Components
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React, { type FC } from 'react'
+
+export interface ButtonProps {
+	label: string
+}
+
+const Button: FC<ButtonProps> = ({ label }) => {
+	return <button>{label}</button>
+}
+
+export default Button
